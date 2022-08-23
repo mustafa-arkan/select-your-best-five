b690f3d484cc2a90dcde8efe90cadab4e8e0917e
mustafa-arkan <arkancse@gmail.com>
2022-08-23 11:09:28 +0600

    total player cost done

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ const playerList=document.createElement('li')
 
 const limit=selectedPlayers.childNodes.length
 
+
 playerList.innerHTML=playerNameValue
 
 if(limit >=0 && limit <=5){
@@ -42,13 +43,74 @@ selectButtons[i].style.backgroundColor=' '
 }
 
 
+
+
+
+
+
+///////
+
+
+
+////////
+
    })
 
 }
 
+//////////////end of selected-v section
 
 
 
+
+
+document.getElementById('btn-perPlayerCalc').addEventListener('click',function(){
+
+
+   const perPlayerCost=document.getElementById('perPlayer-field')
+   const perPlayerCostString=perPlayerCost.value
+   
+   newPerPlayerCost=parseFloat(perPlayerCostString)
+   //console.log(newPerPlayerCost)
+//get per player field value.
+
+const selPlayers=document.getElementById('selected-v')
+
+const pist=document.createElement('li')
+
+const lengthy=selPlayers.childNodes.length-1
+
+const amountForTotalPlayer=lengthy*newPerPlayerCost
+  
+   //console.log(amountForTotalPlayer)
+
+   //length tekhe length number nia perplayer er cost gun
+
+   const playerCost=document.getElementById('perPlayer')
+
+   const previousCostString=playerCost.innerText
+
+
+   const previousCost=parseFloat(previousCostString)
+
+   
+   perPlayerCost.value=' '
+
+   //get the cuurent player expense
+
+   //console.log(previousCostString)
+
+   const currentCost=amountForTotalPlayer
+
+   //console.log(currentCost)
+
+   playerCost.innerText=currentCost
+
+
+//total player cost done
+
+   
+   })
 
 
 
